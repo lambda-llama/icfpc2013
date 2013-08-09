7b1e78b65f6bcb57f953c038f283fb914ad9eb19
Sergei Lebedev <superbobry@gmail.com>
2013-08-09 14:44:45 +0400

    Stylish generator!

--- src/Language/BV/Gen.hs
+++ src/Language/BV/Gen.hs
@@ -2,26 +2,41 @@
 
 module Language.BV.Gen where
 
-import Language.BV.Types
+import Data.Maybe (catMaybes)
 import qualified Data.Map as Map
-import Data.Maybe(catMaybes)
 
+import Language.BV.Types
 import Language.BV.Util
-genexpr :: [String] -> Int -> [BVExpr]
-genexpr ops =
+
+genExpr :: [String] -> Int -> [BVExpr]
+genExpr ops =
     let
-        specgen = genexpr ops
+        specgen = genExpr ops
         op1s = catMaybes $ map op1ByTag ops
         op2s = catMaybes $ map op2ByTag ops
         ifs = catMaybes $ map ifByTag ops
-        m = Map.fromList [(i, go i) | i <- [1..10]]
-        go i =
-            case i of
-                1 -> [Zero, One, Id "x"]
-                otherwise -> [Op1 op1 x | op1 <- op1s, x <- specgen(i - 1)] ++
-                             [Op2 op2 x y | op2 <- op2s, j <- [1..i-1], k <- [1..i-1],
-                              x <- (specgen j), y <- (specgen k), x >= y, j + k == i - 1] ++
-                             [if_ x y z | if_ <- ifs, j <- [1..i-1], k <- [1..i-1], l <- [1..i-1],
-                              x <- (specgen j), y <- (specgen k), z <- (specgen l), j + k + l == i - 1]
-    in
-     \i -> m Map.! i
+        m = Map.fromList [(i, go i) | i <- [1..42]]
+        go 1  = [Zero, One, Id "x"]
+        go _i = [ Op1 op1 x
+                | op1 <- op1s
+                , x   <- specgen (i - 1)
+                ] ++
+                [ Op2 op2 x y
+                | op2 <- op2s
+                , j  <- [1..i-1]
+                , k  <- [1..i-1]
+                , x  <- specgen j
+                , y  <- specgen k
+                , x >= y, j + k == i - 1
+                ] ++
+                [ if_ x y z
+                | if_ <- ifs
+                , j <- [1..i-1]
+                , k <- [1..i-1]
+                , l <- [1..i-1]
+                , x <- specgen j
+                , y <- specgen k
+                , z <- specgen l
+                , j + k + l == i - 1
+                ]
+    in \size -> m Map.! (pred size) -- -1 because of the outer \.

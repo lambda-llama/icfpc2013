877bea422efcacedf144f187ce3228c3dc2e35e5
zmactep <zhmactep@gmail.com>
2013-08-09 10:47:59 +0400

    Type hotfix

--- src/Language/BV/Eval.hs
+++ src/Language/BV/Eval.hs
@@ -8,7 +8,7 @@ import Language.BV.Types
 evalBv :: BVProgram -> Int64 -> Word64
 evalBv (BVProgram (x, e)) v = evalBvExpr e [(x, v)]
 
-evalBvExpr :: BVExpr -> [(BVId, Int64)] -> Word64
+evalBvExpr :: BVExpr -> [(BVId, Word64)] -> Word64
 evalBvExpr e env = case e of
     Zero -> 0
     One  -> 1
@@ -24,14 +24,14 @@ evalBvExpr e env = case e of
     Op1 op1 e1    -> evalOp1 op1 e1 env
     Op2 op2 e1 e2 -> evalOp2 op2 e1 e2 env
 
-evalOp1 :: BVOp1 -> BVExpr -> [(BVId, Int64)] -> Int64
---evalOp1 Not e env = .
---evalOp1 Shl1 e env = .
---evalOp1 Shr1 e env = .
---evalOp1 Shr4 e env = .
---evalOp1 Shr8 e env = .
+evalOp1 :: BVOp1 -> BVExpr -> [(BVId, Word64)] -> Word64
+evalOp1 Not e env = 
+evalOp1 Shl1 e env = .
+evalOp1 Shr1 e env = .
+evalOp1 Shr4 e env = .
+evalOp1 Shr8 e env = .
 
-evalOp2 :: BVOp2 -> BVExpr -> BVExpr -> [(BVId, Int64)] -> Int64
+evalOp2 :: BVOp2 -> BVExpr -> BVExpr -> [(BVId, Word64)] -> Word64
 evalOp2 And e1 e2 env = (evalByExpr e1 env) .&. (evalByExpr e2 env)
 evalOp2 Or e1 e2 env = (evalByExpr e1 env) .|. (evalByExpr e2 env)
 evalOp2 Xor e1 e2 env = xor (evalByExpr e1 env) (evalByExpr e2 env)

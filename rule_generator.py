48f0004a5699e265b94394cc396525897d370969
zmactep <zhmactep@gmail.com>
2013-08-10 15:41:57 +0400

    Ugly hack

---
+++ rule_generator.py
@@ -0,0 +1,24 @@
+import itertools
+
+def f(it):
+    s = ""
+    for i in it:
+        s += "(Op1 %s " % i
+    s += "e"
+    for i in it:
+        s += ")"
+    return s
+    
+
+m = {}
+l = ["Shr1", "Shr4", "Shr16"]
+for i in range(2, 10):
+    for r in itertools.combinations_with_replacement(l, i):
+        m[tuple(r)] = sorted(r, key=(lambda x: int(x[3:])))
+
+for rule in m:
+    s = "go "
+    s += f(rule)
+    s += " = Right "
+    s += f(m[rule])
+    print(s)

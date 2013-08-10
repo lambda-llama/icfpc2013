637ad082dc4f6b052d78cd2469d82f68cb446bc4
zmactep <zhmactep@gmail.com>
2013-08-10 15:53:12 +0400

    Sorted rules

--- rule_generator.py
+++ rule_generator.py
@@ -16,9 +16,14 @@ for i in range(2, 10):
     for r in itertools.combinations_with_replacement(l, i):
         m[tuple(r)] = sorted(r, key=(lambda x: int(x[3:])))
 
+ll = []
 for rule in m:
     s = "go "
     s += f(rule)
     s += " = Right "
     s += f(m[rule])
-    print(s)
+    ll.append(s)
+
+ll = sorted(ll, key=(lambda x: x.count("(")), reverse=True)
+for i in ll:
+    print(i)

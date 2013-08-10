7ae9c5e0817dd1bd38142d7d240a99f2c04b24c2
matklad <aleksey.kladov@gmail.com>
2013-08-10 10:07:57 +0400

    move some defs somehere

--- src/Language/BV/Symbolic/Types.hs
+++ src/Language/BV/Symbolic/Types.hs
@@ -8,25 +8,3 @@ type Sword = [Sbit]
            
 data Sbit = Zero|One|B Int8|Bot
           deriving (Eq, Show, Ord)
-
-
-liftop1 :: (a -> a) -> (Sbit -> Sbit)
-liftop1 = undefined
-
-liftop2 :: (a -> a -> a) -> (Sbit -> Sbit-> Sbit)
-liftop2 = undefined
-
-seval :: BVProgram -> Sword
-seval = undefined
-
-
-not   = undefined
-shl1  = undefined
-shr1  = undefined
-shr4  = undefined
-shr16 = undefined
-    
-and  = undefined
-or   = undefined
-xor  = undefined
-plus = undefined

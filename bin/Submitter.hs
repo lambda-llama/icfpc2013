60c741b35e8a6827c14819e8805aa5d9d75b32ef
Sergei Lebedev <superbobry@gmail.com>
2013-08-11 18:47:57 +0400

    Generator is no more, long live Submitter!

--- bin/Submitter.hs
+++ bin/Submitter.hs
@@ -1,4 +1,8 @@
+{-# OPTIONS_GHC -funbox-strict-fields #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -6,19 +10,30 @@ import Prelude hiding (id)
 
 import Control.Applicative ((<$>))
 import Control.Concurrent (threadDelay)
-import Data.Maybe (fromMaybe)
+import Control.Monad (forever)
+import Data.Bits (bit)
+import Data.Maybe (fromMaybe, fromJust)
 import Data.Word (Word64)
+import System.Random (getStdGen, randoms)
 import Text.Printf (printf)
 import qualified Control.Exception as E
 
+import Control.Parallel.Strategies (using, parBuffer, rseq)
 import Data.Aeson ((.=), (.:), decode', encode, object)
 import Data.Aeson.Types (Object, Pair, parseMaybe)
+import Data.Hashable (Hashable(..))
 import Network.HTTP.Conduit (HttpException(..), RequestBody(..),
                              requestBody, responseBody, method,
                              parseUrl, withManager, httpLbs)
 import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector.Unboxed as VU
 
-import Language.BV.Types (BVProgram)
+import Language.BV.Eval (evalExpr)
+import Language.BV.Gen (genExpr)
+import Language.BV.Types (BVExpr, BVProgram(..))
+
+instance Hashable (VU.Vector Word64) where
+    hashWithSalt = VU.foldl' hashWithSalt
 
 secret :: String
 secret = "0047qni4Jv0ErZcl53WtPweKjim6oLQ6dqDLc9SzvpsH1H"
@@ -35,24 +50,38 @@ unsafeSendRequest uri o = do
 
 sendRequest :: String -> [Pair] -> IO Object
 sendRequest uri o =
-    unsafeSendRequest uri o
-    `E.catch`
-    \(StatusCodeException status _hdrs _cookies) -> do
-        print status
-        threadDelay 2000
+    unsafeSendRequest uri o `E.catch` \(e :: HttpException) -> do
+        print e
+        threadDelay 4000
         sendRequest uri o
 
-evalID :: String -> [Int] -> IO (Maybe [Word64])
+evalID :: String -> [Word64] -> IO [Word64]
 evalID id input = do
     result <- sendRequest "eval"
               [ "id"        .= id
               , "arguments" .= (map (printf "0x%X") input :: [String])
               ]
-    return $! map read <$> parseMaybe (.: "outputs") result
+    return $! map read . fromJust $ parseMaybe (.: "outputs") result
 
-guess :: String -> BVProgram -> IO Object
-guess id program =
-    sendRequest "guess" ["id" .= id, "program" .= show program]
+data Status = Win
+            | Mismatch !(Word64, Word64, Word64)
+            | Error !String
+    deriving (Show, Eq)
+
+guess :: String -> BVProgram -> IO Status
+guess id program = do
+    result <- sendRequest "guess" ["id" .= id, "program" .= show program]
+    return $! fromJust $ do
+        status <- parseMaybe (.: "status") result
+        case status :: String of
+            "win"      -> return Win
+            "mismatch" -> do
+                values <- map read <$> parseMaybe (.: "values") result
+                return $ case values of
+                    [input, output, answer] -> Mismatch (input, output, answer)
+                    _other -> error "The impossible happened!"
+            "error"    -> Error <$> parseMaybe (.: "error") result
+            _          -> error "The impossible happened!"
 
 
 data TrainRequest  = Size Int | Operators [String]
@@ -63,8 +92,48 @@ train (Operators ops) = sendRequest "train" ["operators" .= ops]
 
 
 main :: IO ()
-main = do
-    result <- train $ Size 30
-    case parseMaybe (.: "id") result of
-        Just id -> evalID id [42] >>= print
-        Nothing -> error ":("
+main = fake where
+  fake :: IO ()
+  fake = forever $ do
+    response <- train (Size 10)
+    let id   = fromJust $ parseMaybe (.: "id") response
+        size = fromJust $ parseMaybe (.: "size") response
+        ops  = fromJust $ parseMaybe (.: "operators") response
+    solve id size ops
+
+  real :: IO ()
+  real = do
+    id   <- getLine
+    size <- read <$> getLine
+    ops  <- read <$> getLine
+    solve id size ops
+
+solve :: String -> Int -> [String] -> IO ()
+solve id size ops = do
+    r <- getStdGen
+    let !inputs = meaningfulInputs ++
+                  (take (256 - length meaningfulInputs) $ randoms r)
+        !envs   = map (\x -> [('x', x)]) inputs
+        !eqCls  = HashMap.fromListWith (\[expr] rest -> expr : rest) $!
+                  ([ (VU.fromListN 256 $ map (\env -> evalExpr env expr) envs,
+                      [expr])
+                   | expr <- genExpr ops (pred size)
+                   ] `using` parBuffer (bit 32) rseq)
+
+    outputs <- evalID id inputs
+    bruteForce $ eqCls HashMap.! VU.fromList outputs
+  where
+    meaningfulInputs :: [Word64]
+    meaningfulInputs = 42 : map bit [0..64]
+
+    bruteForce :: [BVExpr] -> IO ()
+    bruteForce [] = error "Exausted candidate list :("
+    bruteForce (candidate:exprs) = do
+        status <- guess id (BVProgram ('x', candidate))
+        print (candidate, status)
+        case status of
+            Win -> return ()
+            Mismatch (input, output, _answer) ->
+                bruteForce $!
+                filter (\expr -> evalExpr [('x', input)] expr == output) exprs
+            Error reason -> error reason

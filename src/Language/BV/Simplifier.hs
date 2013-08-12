1e1a4809b6ccc1bc69e58022c9de5f48fe222a7d
Sergei Lebedev <superbobry@gmail.com>
2013-08-12 15:26:17 +0400

    Cleaned up 'Language.BV.Simplifier'

--- src/Language/BV/Simplifier.hs
+++ src/Language/BV/Simplifier.hs
@@ -40,94 +40,97 @@ import Language.BV.Symbolic.Operations (isZero, isNotZero)
 -- (plus e e)              = (shl1 e)
 
 isNotRedundant :: [String] -> BVExpr -> Bool
-isNotRedundant context !e =
+isNotRedundant ctx !e =
     not $
-    isNotNot e context ||
-    isShr e context ||
-    isOp2Zero e context ||
-    isLogicNotZero e context || -- good
-    isWrongOrder e context ||   -- good
-    isSameIfStart e context ||  -- good
-    isDeMorgan e context ||     -- good
-    isDrunk e context ||          -- good
-    isPlusShr e context ||
-    isClosedFold e context ||   -- good
-    isLogicRepeat e context ||
-    isTrivialIf e context ||
-    isSlike e context        -- good
+    isNotNot e ||
+    isShr e ctx ||
+    isOp2Zero e ||
+    isLogicNotZero e ||
+    isWrongOrder e ||
+    isSameIfStart e ||
+    isDeMorgan e ctx ||
+    isDrunk e ctx ||
+    isPlusShr e ctx ||
+    isClosedFold e ctx ||
+    isLogicRepeat e ||
+    isTrivialIf e ctx ||
+    isSlike e ctx
 {-# INLINE isNotRedundant #-}
 
-isNotNot :: BVExpr -> [String] -> Bool
-isNotNot (Op1 Not (Op1 Not _)) _ = True
-isNotNot _ _ = False
+isNotNot :: BVExpr -> Bool
+isNotNot (Op1 Not (Op1 Not _e1)) = True
+isNotNot _ =  False
 {-# INLINE isNotNot #-}
 
 isShr :: BVExpr -> [String] -> Bool
-isShr (Op1 Shr1 (Op1 Shr1 (Op1 Shr1 (Op1 Shr1 _)))) context = "shr4" `elem` context  -- shr4
-isShr (Op1 Shr4 (Op1 Shr4 (Op1 Shr4 (Op1 Shr4 _)))) context = "shr16" `elem` context -- shr16
-isShr _ _ = False
+isShr (Op1 Shr1 (Op1 Shr1 (Op1 Shr1 (Op1 Shr1 _)))) = ("shr4" `elem`)
+isShr (Op1 Shr4 (Op1 Shr4 (Op1 Shr4 (Op1 Shr4 _)))) = ("shr16" `elem`)
+isShr _e = const False
 {-# INLINE isShr #-}
 
 isPlusShr :: BVExpr -> [String] -> Bool
-isPlusShr (Op2 Plus e1 (Op2 Plus e2 _)) context = e1 `like` e2 && "shl1" `elem` context -- shl1
-isPlusShr (Op2 Plus e1 e2) context              = e1 `like` e2 && "shl1" `elem` context -- shl1
-isPlusShr _ _ = False
+isPlusShr (Op2 Plus e1 (Op2 Plus e2 _e3)) ctx =
+    e1 `like` e2 && "shl1" `elem` ctx
+isPlusShr (Op2 Plus e1 e2) ctx                =
+    e1 `like` e2 && "shl1" `elem` ctx
+isPlusShr _e _ctx = False
 {-# INLINE isPlusShr #-}
 
 
 isTrivialIf :: BVExpr -> [String] -> Bool
-isTrivialIf (If0 e0 e1 e2) _ =
-    e0 `like` e1
+isTrivialIf (If0 e0 e1 e2) _ctx =
+       e0 `like` e1
     || e1 `like` e2
-    ||  let !res = sevalExprStd e0
-        in isZero res || isNotZero res
-isTrivialIf _ _ = False
+    || let !res = sevalExprStd e0 in isZero res || isNotZero res
+isTrivialIf _e _ctx = False
 {-# INLINE isTrivialIf #-}
 
-isOp2Zero :: BVExpr -> [String] -> Bool
-isOp2Zero (Op2 _ _ Zero) _  = True
-isOp2Zero (Op2 _ Zero _) _  = True
-isOp2Zero _ _ = False
+isOp2Zero :: BVExpr -> Bool
+isOp2Zero (Op2 _e0 _e1 Zero)  = True
+isOp2Zero (Op2 _op2 Zero _e1) = True
+isOp2Zero _e = False
 {-# INLINE isOp2Zero #-}
 
-isLogicRepeat :: BVExpr -> [String] -> Bool
-isLogicRepeat (Op2 And e1 e2) _ = e1 `like` e2
-isLogicRepeat (Op2 Or e1 e2) _  = e1 `like` e2
-isLogicRepeat (Op2 Xor e1 e2) _ = e1 `like` e2
-isLogicRepeat (Op2 op1 e1 (Op2 op2 e2 _)) _ = op1 == op2 && e1 `like` e2
-isLogicRepeat _ _ = False
+isLogicRepeat :: BVExpr -> Bool
+isLogicRepeat (Op2 And e1 e2) = e1 `like` e2
+isLogicRepeat (Op2 Or e1 e2)  = e1 `like` e2
+isLogicRepeat (Op2 Xor e1 e2) = e1 `like` e2
+isLogicRepeat (Op2 op1 e1 (Op2 op2 e2 _)) = op1 == op2 && e1 `like` e2
+isLogicRepeat _e = False
 {-# INLINE isLogicRepeat #-}
 
-isLogicNotZero :: BVExpr -> [String] -> Bool
-isLogicNotZero (Op2 _ _ (Op1 Not Zero)) _ = True
-isLogicNotZero (Op2 _ (Op1 Not Zero) _) _ = True
-isLogicNotZero _ _ = False
+isLogicNotZero :: BVExpr -> Bool
+isLogicNotZero (Op2 op2 _e0 (Op1 Not Zero)) = op2 `elem` [And, Or, Xor]
+isLogicNotZero (Op2 op2 (Op1 Not Zero) _e3) = op2 `elem` [And, Or, Xor]
+isLogicNotZero _e = False
 {-# INLINE isLogicNotZero #-}
 
-isWrongOrder :: BVExpr -> [String] -> Bool
-isWrongOrder (Op2 op1 e1 (Op2 op2 e2 _)) _ = op1 == op2 && e1 > e2
-isWrongOrder (Op2 _ e1 e2) _               = e1 > e2
-isWrongOrder _ _ = False
+isWrongOrder :: BVExpr -> Bool
+isWrongOrder (Op2 op1 e1 (Op2 op2 e2 _e3)) = op1 == op2 && e1 > e2
+isWrongOrder (Op2 _op1 e1 e2)              = e1 > e2
+isWrongOrder _e = False
 {-# INLINE isWrongOrder #-}
 
-isSameIfStart :: BVExpr -> [String] -> Bool
-isSameIfStart (If0 _ (Op1 op1 _) (Op1 op2 _)) _     = op1 == op2
-isSameIfStart (If0 _ (Op2 op1 _ _) (Op2 op2 _ _)) _ = op1 == op2
-isSameIfStart _ _ = False
+isSameIfStart :: BVExpr -> Bool
+isSameIfStart (If0 _e0 (Op1 op1 _e1) (Op1 op2 _e2)) = op1 == op2
+isSameIfStart (If0 _e0 (Op2 op1 _e1 _e2) (Op2 op2 _e3 _e4)) = op1 == op2
+isSameIfStart _e = False
 {-# INLINE isSameIfStart #-}
 
 isDeMorgan :: BVExpr -> [String] -> Bool
-isDeMorgan (Op2 Or (Op1 Not _) (Op1 Not _)) context  = "and" `elem` context -- and
-isDeMorgan (Op2 And (Op1 Not _) (Op1 Not _)) context = "or" `elem` context -- or
-isDeMorgan _ _ = False
+isDeMorgan (Op2 Or (Op1 Not _) (Op1 Not _))  = ("and" `elem`)
+isDeMorgan (Op2 And (Op1 Not _) (Op1 Not _)) = ("or" `elem`)
+isDeMorgan _e = const False
 {-# INLINE isDeMorgan #-}
 
 isClosedFold :: BVExpr -> [String] -> Bool
-isClosedFold e@(Fold (BVFold _ i (_, _, body))) context = ('y' `notElem` freeVars body &&
-                                                           "shr16" `elem` context && "shr4" `elem` context) ||
-                                                          isClosed body ||
-                                                          e `like` i
-isClosedFold _ _ = False
+isClosedFold e@(Fold (BVFold _ i (_, _, body))) ctx =
+    ('y' `notElem` freeVars body &&
+     "shr16" `elem` ctx &&
+     "shr4"  `elem` ctx) ||
+    isClosed body ||
+    e `like` i
+isClosedFold _e _ctx = False
 {-# INLINE isClosedFold #-}
 
 slikeConsts1 :: [(BVExpr, Sword)]
@@ -145,16 +148,18 @@ slikeConsts4 :: [(BVExpr, Sword)]
 slikeConsts4 = [(expr, sevalExprStd expr) | expr <- [Op1 Shr16 (Op1 Not One)]]
 
 isSlike :: BVExpr -> [String] -> Bool
-isSlike e context = f slikeConsts1 ||
-                    (f slikeConsts2 && ("not" `elem` context)) ||
-                    (f slikeConsts3 && ("shr16" `elem` context)) ||
-                    (f slikeConsts4 && ("not" `elem` context) && ("shr16" `elem` context)) -- not shr16
-  where f  = any (\(c, s) -> e /= c && slike se s)
-        se = sevalExprStd e
+isSlike e ctx =
+    f slikeConsts1 ||
+    (f slikeConsts2 && ("not" `elem` ctx)) ||
+    (f slikeConsts3 && ("shr16" `elem` ctx)) ||
+    (f slikeConsts4 && ("not" `elem` ctx) && ("shr16" `elem` ctx))
+  where
+    f  = any (\(c, s) -> e /= c && slike se s)
+    se = sevalExprStd e
 {-# INLINE isSlike #-}
 
 isDrunk :: BVExpr -> [String] -> Bool
-isDrunk (Op2 And _ (Op1 Not One)) context = ("shr1" `elem` context) && ("shl1" `elem` context) -- shr1 shl1
-isDrunk (Op2 And (Op1 Not One) _) context = ("shr1" `elem` context) && ("shl1" `elem` context) -- shr1 shl1
-isDrunk _ _ = False
+isDrunk (Op2 And _e0 (Op1 Not One)) ctx = "shr1" `elem` ctx && "shl1" `elem` ctx
+isDrunk (Op2 And (Op1 Not One) _e2) ctx = "shr1" `elem` ctx && "shl1" `elem` ctx
+isDrunk _e _cts = False
 {-# INLINE isDrunk #-}
